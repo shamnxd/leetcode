6bc4c17a950200cc7b717ea1e4ec78ec08117101
Specxzy <87613154+shamnxd@users.noreply.github.com>
2025-12-08 21:19:23 +0530

    Time: 338 ms (15.38%), Space: 54.3 MB (71.15%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function (n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            for (let k = 1; k <= n; k++) {
+                if ((i * i) + (j * j) == k * k) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+};
