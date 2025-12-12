36fb6b3a72a1e52a1f42a38bf0fc30b04635d985
Specxzy <87613154+shamnxd@users.noreply.github.com>
2025-12-12 22:40:56 +0530

    Time: 52 ms (41.08%), Space: 54 MB (43.23%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+    return Math.floor((high - low) / 2) + ((low % 2 === 1 || high % 2 === 1) ? 1 : 0);
+};
