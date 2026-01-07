b7f229e20c483314055a57383f9cfd202a290402
Specxzy <87613154+shamnxd@users.noreply.github.com>
2026-01-07 20:06:56 +0530

    Time: 1 ms (56.76%), Space: 56 MB (36.94%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfPairs = function (nums1, nums2, k) {
+    let count = 0;
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            let value = nums2[j] * k;
+            if (nums1[i] % value === 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
