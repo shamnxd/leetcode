536e394d9ac9d19ba177108332c98a45d8a69de8
Specxzy <87613154+shamnxd@users.noreply.github.com>
2026-01-09 12:21:14 +0530

    Time: 0 ms (100%), Space: 57.6 MB (5.66%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
@@ -6,11 +6,10 @@
 var minOperations = function (nums, k) {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= k) {
-            console.log(nums[i])
+        if (nums[i] < k) {
             count++;
         }
     }
 
-    return nums.length - count;
+    return count;
 };
