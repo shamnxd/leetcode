d9ad2e4cb76fc30d33d5a60343ed0f083dfa7880
Specxzy <87613154+shamnxd@users.noreply.github.com>
2026-03-04 23:39:03 +0530

    Time: 0 ms (100%), Space: 57.1 MB (26.2%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var sumOfMultiples = function(n) {
-    let sum = 0
-    for(let i =1 ; i<=n;i++){
-        if(i%3==0||i%5==0||i%7==0){
-            sum+=i
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+            sum += i;
         }
     }
-    return sum
+
+    return sum;
 };
