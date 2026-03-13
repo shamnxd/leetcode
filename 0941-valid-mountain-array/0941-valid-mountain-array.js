0243c9697f9c57083706cb3f20ac9415595d63f6
Shamnad T <87613154+shamnxd@users.noreply.github.com>
2026-03-13 15:16:56 +0530

    Time: 41 ms (89.7%), Space: 57 MB (77.8%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function (arr) {
+    let n = arr.length;
+    if (n < 3) return false;
+
+    let i = 0;
+
+    while (i + 1 < n && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+    if (i == 0 || i == n - 1) return false;
+
+    while (i + 1 < n && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
+};
