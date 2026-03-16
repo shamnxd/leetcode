811682c17060a6038379f127f08ae835f793a924
Muhammad Shamnad T <87613154+shamnxd@users.noreply.github.com>
2026-03-16 21:32:09 +0530

    Time: 18 ms (59.94%), Space: 59 MB (54.37%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,22 +4,15 @@
  * @return {boolean}
  */
 var canConstruct = function (ransomNote, magazine) {
-
-    let rMap = {};
-    let mMap = {};
-
-    for (let ch of ransomNote) {
-        rMap[ch] = (rMap[ch] || 0) + 1;
-    }
+    let count = {};
 
     for (let ch of magazine) {
-        mMap[ch] = (mMap[ch] || 0) + 1;
+        count[ch] = (count[ch] || 0) + 1;
     }
 
-    for (let key in rMap) {
-        if (!mMap[key] || mMap[key] < rMap[key]) {
-            return false;
-        }
+    for (let ch of ransomNote) {
+        if (!count[ch]) return false;
+        count[ch]--;
     }
 
     return true;
