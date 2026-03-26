623593a7cd7e08c2ed1d44d9a21085cf007f80d0
Muhammad Shamnad T <87613154+shamnxd@users.noreply.github.com>
2026-03-26 09:27:23 +0530

    Time: 0 ms (100%), Space: 55.2 MB (5.56%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -9,12 +9,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var middleNode = function(head) {
-    let slow = head
-    let fast = head
+var middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+
     while (fast && fast.next) {
-        slow = slow.next
-        fast = fast.next.next
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    return slow
+
+    return slow;
 };
