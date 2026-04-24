e6ba22372d575ea7da2a6cecd9c1585d5c9e3476
Muhammad Shamnad T <87613154+shamnxd@users.noreply.github.com>
2026-04-24 19:30:02 +0530

    Time: 2 ms (97.96%), Space: 58 MB (47.96%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortEvenOdd = function(nums) {
+    let even = [];
+    let odd = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            even.push(nums[i]);
+        } else {
+            odd.push(nums[i]);
+        }
+    }
+
+    even.sort((a, b) => a - b);
+
+    odd.sort((a, b) => b - a);
+
+    let e = 0, o = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            nums[i] = even[e++];
+        } else {
+            nums[i] = odd[o++];
+        }
+    }
+
+    return nums;
+};
